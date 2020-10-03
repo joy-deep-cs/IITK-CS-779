1f8e1a3d257604740cfea27ac3a8677f15ba6659
Joydeep <joydeep20@iitk.ac.in>
2020-10-03 13:55:34 +0530

    virtual environment with dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 pip install -r resources/requirement.txt
-python -m src.utility.setup.py
+pipenv install
+pipenv run python -m src.utility.setup.py
 rm temp/*
 git clone https://github.com/harperco/MeasEval.git temp/masEval/
 cp -r temp/masEval/data/* data/
--- src/data/data_visualization.py
+++ src/data/data_visualization.py
@@ -9,25 +9,29 @@ Original file is located at
 
 # !git clone https://github.com/harperco/MeasEval.git
 
+from ..utility.Utility import Utility
+
+allDirs = Utility.getAllDir()
+
 import glob
 import pandas as pd
 
-df = pd.concat(map(pd.read_csv, glob.glob('/content/MeasEval/data/trial/tsv/*.tsv')))
+# df = pd.concat(map(pd.read_csv, glob.glob(allDirs["data"]+'/trial/tsv/*.tsv')))
 
-from google.colab import drive
-drive.mount('/content/drive')
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 import pandas as pd
-df =  pd.read_csv('/content/MeasEval/data/trial/tsv/S0019103512002801-1342.tsv',delimiter='\t',engine='python')
+df =  pd.read_csv(allDirs["data"]+'trial/tsv/S0019103512002801-1342.tsv',delimiter='\t',engine='python')
 df = df.sort_values(by = 'startOffset')
-df
+# df
 
 
 
 import spacy
 from spacy import displacy
 from IPython.core.display import display, HTML
-#text = open('/content/MeasEval/data/trial/txt/S0019103512002801-1342.txt', 'r').read()
+text = open(allDirs["data"]+'/trial/txt/S0019103512002801-1342.txt', 'r').read()
 df.columns = ['docId', 'annotSet', 'label', 'start', 'end','annotId','text','other' ]
 ents= df[['label','start','end']].to_dict(orient='records')
 
@@ -39,13 +43,8 @@ display(HTML(html))
 
 import os
 
-list = os.listdir('/content/MeasEval/data/trial/txt') # dir is your directory path
+list = os.listdir(allDirs["data"]+'/trial/txt') # dir is your directory path
 number_files = len(list)
-number_files
+print(number_files)
 
 text= 'Rhea is Saturn’s largest icy moon (radius: 1RRh = 764 km). It orbits the planet on an equatorial and circular orbit at a distance of about 8.74Rs from its center (1Rs = 60,268 km).'
-
-text
-
-text
-
