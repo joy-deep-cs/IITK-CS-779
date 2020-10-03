6b5780dc2dfeffe9145aefb8978b5504566e32d3
Joydeep <joydeep20@iitk.ac.in>
2020-10-03 12:41:39 +0530

    data download

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,7 @@
 pip install -r resources/requirement.txt
 python -m src.utility.setup.py
+rm temp/*
+git clone https://github.com/harperco/MeasEval.git temp/masEval/
+cp -r temp/masEval/data/* data/
+rm -r temp/*
+rm temp/*
