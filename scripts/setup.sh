fef5715b1b993edeb392dff853e3b5065b24afc3
Joydeep <joydeep20@iitk.ac.in>
2020-10-07 17:13:55 +0530

    grobid pretrained model

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,15 +7,19 @@ cp -r temp/masEval/data/* data/
 rm -r temp/*
 rm temp/*
 wget -P temp/ https://github.com/kermitt2/grobid/archive/0.6.1.zip
-unzip temp/0.6.1.zip -d resources/
+unzip temp/0.6.1.zip -d resources/grobid/
 rm temp/0.6.1.zip
-cd resources/grobid-0.6.1
+cd resources/grobid/grobid-0.6.1
 ./gradlew clean install
+cd ..
 cd ..
 cd ..
 wget -P temp/ https://github.com/kermitt2/grobid-quantities/archive/master.zip
-unzip temp/master.zip -d resources/grobid-0.6.1/
+unzip temp/master.zip -d resources/grobid/grobid-0.6.1/
 rm temp/master.zip
-cd resources/grobid-0.6.1/grobid-quantities-master
+cp resources/gradle-wrapper.properties resources/grobid/grobid-0.6.1/grobid-quantities-master/gradle/wrapper/
+cd resources/grobid/grobid-0.6.1/grobid-quantities-master
 ./gradlew copyModels
 ./gradlew clean install
+# java -jar build/libs/grobid-quantities-0.6.1-SNAPSHOT-onejar.jar trainingGeneration resources/config/config.yml -dIn ../../../../data/train/text/ -dOut ../../../../output/
+java -jar build/libs/grobid-quantities-0.6.1-SNAPSHOT-onejar.jar server resources/config/config.yml
