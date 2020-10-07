38116d756f407615d3629468ed813e24d71bfde6
Joydeep <joydeep20@iitk.ac.in>
2020-10-07 17:53:27 +0530

    setup gobrid's quantitiy module

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## CS-779 Course Project
 1. Make sure you have linux operating system
 1. Make sure you have python (3.8 preferably) installed 
+1. Make sure you have Java (Open JDK version >=15.0)
 1. Make sure you have pip enabled for all the users
 1. clone this repo
     1. cd IITK-CS-779
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,20 +6,3 @@ git clone https://github.com/harperco/MeasEval.git temp/masEval/
 cp -r temp/masEval/data/* data/
 rm -r temp/*
 rm temp/*
-wget -P temp/ https://github.com/kermitt2/grobid/archive/0.6.1.zip
-unzip temp/0.6.1.zip -d resources/grobid/
-rm temp/0.6.1.zip
-cd resources/grobid/grobid-0.6.1
-./gradlew clean install
-cd ..
-cd ..
-cd ..
-wget -P temp/ https://github.com/kermitt2/grobid-quantities/archive/master.zip
-unzip temp/master.zip -d resources/grobid/grobid-0.6.1/
-rm temp/master.zip
-cp resources/gradle-wrapper.properties resources/grobid/grobid-0.6.1/grobid-quantities-master/gradle/wrapper/
-cd resources/grobid/grobid-0.6.1/grobid-quantities-master
-./gradlew copyModels
-./gradlew clean install
-# java -jar build/libs/grobid-quantities-0.6.1-SNAPSHOT-onejar.jar trainingGeneration resources/config/config.yml -dIn ../../../../data/train/text/ -dOut ../../../../output/
-java -jar build/libs/grobid-quantities-0.6.1-SNAPSHOT-onejar.jar server resources/config/config.yml
